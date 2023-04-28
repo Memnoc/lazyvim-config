4f1d09e03a824838e5eb3919020c65480bea2c36
Memnoc <smartdroidesign@gmail.com>
2023-04-28 05:54:27 +0100

    config 28-04-2023

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,22 @@
+return {
+  -- add nord theme
+  --{ "shaunsingh/nord.nvim" },
+  { "catppuccin/nvim" },
+  -- { "navarasu/onedark.nvim" },
+  --{"AlexvZyl/nordic.nvim"},
+
+  -- Configure LazyVim to Catppuccin
+  -- {
+  -- 	"LazyVim/LazyVim",
+  -- 	opts = {
+  -- 		colorscheme = "catppuccin-mocha",
+  -- 	},
+  -- },
+
+  { -- Theme inspired by Atom
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
